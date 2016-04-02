9e168bbf9daa5e1d591649cc0bf1014f55db66e0
Celine Bonin <bonin.c.m@gmail.com>
2016-04-02 17:28:39 +1100

    Added binary node class

---
+++ BinaryNode.java
@@ -0,0 +1,33 @@
+
+public class BinaryNode<T> {
+
+	private BinaryNode<T> left;
+	private BinaryNode<T> right;
+	private T value;
+	
+	public  BinaryNode(T node) {
+		this.value = node;
+	}
+
+	public void setLeft(BinaryNode<T> node) {
+		this.left = node;
+	}
+
+	public BinaryNode<T> getLeft(BinaryNode<T> node) {
+		return this.left;
+	}
+	
+	public void setRight(BinaryNode<T> node) {
+		this.right = node;
+	}
+
+	public BinaryNode<T> getRight(BinaryNode<T> node) {
+		return this.right;
+	}
+	
+	public T getValue() {
+		return this.value;
+	}
+	
+	
+}
